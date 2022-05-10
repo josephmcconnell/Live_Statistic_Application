b1a7e02bb462d59fd6a6faa98646ead582cb9c06
dorscher4 <70673423+dorscher4@users.noreply.github.com>
2022-05-10 17:43:34 -0500

    added summary page

--- app/event.service.ts
+++ app/event.service.ts
@@ -14,29 +14,322 @@ export class EventService {
         player: 'CoolCatMeow',
         game_time: '4:21',
         team: 'NDSU'
-    }
+    },
+    {
+      type: 'Shot',
+      game: 'Rocket League',
+      player: 'dorscher',
+      game_time: '3:44',
+      team: 'NDSU'
+    },
+    {
+      type: 'Save',
+      game: 'Rocket League',
+      player: 'dorscher',
+      game_time: '4:21',
+      team: 'NDSU'
+    },
+    {
+      type: 'Shot',
+      game: 'Rocket League',
+      player: 'dorscher',
+      game_time: '4:21',
+      team: 'NDSU'
+    },
+    {
+      type: 'Goal',
+      game: 'Rocket League',
+      player: 'Joe',
+      game_time: '4:21',
+      team: 'NDSU'
+    },
+    {
+      type: 'Save',
+      game: 'Rocket League',
+      player: 'Joe',
+      game_time: '4:21',
+      team: 'NDSU'
+    },
+    {
+      type: 'Save',
+      game: 'Rocket League',
+      player: 'Ktool',
+      game_time: '4:21',
+      team: 'NDSU 2'
+  },
+  {
+    type: 'Shot',
+    game: 'Rocket League',
+    player: 'Ktool',
+    game_time: '3:44',
+    team: 'NDSU 2'
+  },
+  {
+    type: 'Goal',
+    game: 'Rocket League',
+    player: 'TurtleZilla',
+    game_time: '4:21',
+    team: 'NDSU 2'
+  },
+  {
+    type: 'Shot',
+    game: 'Rocket League',
+    player: 'TurtleZilla',
+    game_time: '4:21',
+    team: 'NDSU 2'
+  },
+  {
+    type: 'Goal',
+    game: 'Rocket League',
+    player: 'GreekAvenger',
+    game_time: '4:21',
+    team: 'NDSU 2'
+  },
+  {
+    type: 'Save',
+    game: 'Rocket League',
+    player: 'GreekAvenger',
+    game_time: '4:21',
+    team: 'NDSU 2'
+  }
 ];
 
 getEvents() {
   return this.EVENTS;
 }
 
-getShots(){
+
+//Get stats of Players in Team 1 
+//(Team 1 is the first team in the list of events and player 1 is the first player listed for that team, ect.)
+getStatsRL(){
   let tem: Event[] = this.getEvents();
-    let i: number = 0;
-    let shots: number = 0;
-    let team1: string = tem[0].team;
-    while (i < tem.length) {
-      if(tem[i].team == team1)
-      {
-        if(tem[i].type == 'Shot')
-        {
-          shots = shots + 1;
-        }
-      }
-      i = i + 1;
+  let shots: number = 0;
+  let saves: number = 0;
+  let goals: number = 0;
+  let team1: string = tem[0].team;
+  
+  //Identify player 1
+  let team1player1: string = tem[0].player;
+  let team1player2: string = "";
+  let team1player3: string = "";
+
+  //Identify player 2
+  for(let i = 0; i < tem.length; i++) {
+    if(tem[i].team == team1 && tem[i].player !== team1player1)
+    {
+      team1player2 = tem[i].player;
+      break;
+    }
   }
-  return tem.length;
+
+  //Identify player 3
+  for(let i = 0; i < tem.length; i++) {
+    if(tem[i].team == team1 && tem[i].player !== team1player1 && tem[i].player !== team1player2)
+    {
+      team1player3 = tem[i].player;
+      break;
+    }
+  }
+
+  //Get player 1 stats
+  for (let i = 0; i < tem.length; i++) {
+    if(tem[i].team == team1 && tem[i].player == team1player1)
+    {
+      if(tem[i].type == 'Shot')
+      {
+        shots++;
+      }
+      else if(tem[i].type == 'Save')
+      {
+        saves++;
+      }
+      else if(tem[i].type == 'Goal')
+      {
+        goals++;
+      }
+    }
+  }
+  let team1player1Stats = [team1player1, shots, saves, goals];
+
+  //Reset stat counters
+  shots = 0;
+  saves = 0;
+  goals = 0;
+
+  //Get player 2 stats
+  for (let i = 0; i < tem.length; i++) {
+    if(tem[i].team == team1 && tem[i].player == team1player2)
+    {
+      if(tem[i].type == 'Shot')
+      {
+        shots++;
+      }
+      else if(tem[i].type == 'Save')
+      {
+        saves++;
+      }
+      else if(tem[i].type == 'Goal')
+      {
+        goals++;
+      }
+    }
+  }
+  let team1player2Stats = [team1player2, shots, saves, goals];
+
+  //Reset stat counters
+  shots = 0;
+  saves = 0;
+  goals = 0;
+
+  //Get player 3 stats
+  for (let i = 0; i < tem.length; i++) {
+    if(tem[i].team == team1 && tem[i].player == team1player3)
+    {
+      if(tem[i].type == 'Shot')
+      {
+        shots++;
+      }
+      else if(tem[i].type == 'Save')
+      {
+        saves++;
+      }
+      else if(tem[i].type == 'Goal')
+      {
+        goals++;
+      }
+    }
+  }
+  let team1player3Stats = [team1player3, shots, saves, goals];
+
+  //Collect Team 1 Player Stats
+  let team1Stats = [team1player1Stats, team1player2Stats, team1player3Stats];
+
+
+
+  //Team 2 
+  //Identify Team 2
+  let team2: string = "";
+  for(let i = 0; i < tem.length; i++) {
+    if(tem[i].team !== team1)
+    {
+      team2 = tem[i].team;
+      break;
+    }
+  }
+  
+  //Declare players
+  let team2player1: string = "";
+  let team2player2: string = "";
+  let team2player3: string = "";
+
+//Identify player 1
+for(let i = 0; i < tem.length; i++) {
+  if(tem[i].team == team2)
+  {
+    team2player1 = tem[i].player;
+    break;
+  }
+}
+
+  //Identify player 2
+  for(let i = 0; i < tem.length; i++) {
+    if(tem[i].team == team2 && tem[i].player !== team2player1)
+    {
+      team2player2 = tem[i].player;
+      break;
+    }
+  }
+
+  //Identify player 3
+  for(let i = 0; i < tem.length; i++) {
+    if(tem[i].team == team2 && tem[i].player !== team2player1 && tem[i].player !== team2player2)
+    {
+      team2player3 = tem[i].player;
+      break;
+    }
+  }
+
+  //Reset stat counters
+  shots = 0;
+  saves = 0;
+  goals = 0;
+
+  //Get player 1 stats
+  for (let i = 0; i < tem.length; i++) {
+    if(tem[i].team == team2 && tem[i].player == team2player1)
+    {
+      if(tem[i].type == 'Shot')
+      {
+        shots++;
+      }
+      else if(tem[i].type == 'Save')
+      {
+        saves++;
+      }
+      else if(tem[i].type == 'Goal')
+      {
+        goals++;
+      }
+    }
+  }
+  let team2player1Stats = [team2player1, shots, saves, goals];
+
+  //Reset stat counters
+  shots = 0;
+  saves = 0;
+  goals = 0;
+
+  //Get player 2 stats
+  for (let i = 0; i < tem.length; i++) {
+    if(tem[i].team == team2 && tem[i].player == team2player2)
+    {
+      if(tem[i].type == 'Shot')
+      {
+        shots++;
+      }
+      else if(tem[i].type == 'Save')
+      {
+        saves++;
+      }
+      else if(tem[i].type == 'Goal')
+      {
+        goals++;
+      }
+    }
+  }
+  let team2player2Stats = [team2player2, shots, saves, goals];
+
+  //Reset stat counters
+  shots = 0;
+  saves = 0;
+  goals = 0;
+
+  //Get player 3 stats
+  for (let i = 0; i < tem.length; i++) {
+    if(tem[i].team == team2 && tem[i].player == team2player3)
+    {
+      if(tem[i].type == 'Shot')
+      {
+        shots++;
+      }
+      else if(tem[i].type == 'Save')
+      {
+        saves++;
+      }
+      else if(tem[i].type == 'Goal')
+      {
+        goals++;
+      }
+    }
+  }
+  let team2player3Stats = [team2player3, shots, saves, goals];
+
+  //Collect Team 2 Player Stats
+  let team2Stats = [team2player1Stats, team2player2Stats, team2player3Stats];
+
+  let gamesStats = [team1Stats, team2Stats, team1, team2]
+
+  return gamesStats;
 }
 
 addEvents(game: string, type: string, player: string, game_time: string, team: string) {
--- app/summary/summary.component.ts
+++ app/summary/summary.component.ts
@@ -12,14 +12,22 @@ import { EventService } from '../event.service';
 export class SummaryComponent{
 
 
-  getTotalShots(){
-    return this.eService.getShots();
-  }
-
   htmlYouWantToAdd: string = "";
 
   addHTML(){
-    this.htmlYouWantToAdd = "<b>"+ this.getTotalShots() +"</b>";
+    if(this.eService.EVENTS[0].game == "Rocket League"){
+      this.htmlYouWantToAdd = "<h5>"+ this.eService.getStatsRL()[2] +"</h5><table class=\"table\"><thead><tr><th scope=\"col\">Player</th><th scope=\"col\">Shots</th><th scope=\"col\">Saves</th><th scope=\"col\">Goals</th></tr></thead><tbody><tr><th scope=\"row\">"+ this.eService.getStatsRL()[0][0][0] +"</th><td>"+ this.eService.getStatsRL()[0][0][1] +"</td><td>"+ this.eService.getStatsRL()[0][0][2] +"</td><td>"+ this.eService.getStatsRL()[0][0][3] +"</td></tr><tr><th scope=\"row\">"+ this.eService.getStatsRL()[0][1][0] +"</th><td>"+ this.eService.getStatsRL()[0][1][1] +"</td><td>"+ this.eService.getStatsRL()[0][1][2] +"</td><td>"+ this.eService.getStatsRL()[0][1][3] +"</td></tr><tr><th scope=\"row\">"+ this.eService.getStatsRL()[0][2][0] +"</th><td>"+ this.eService.getStatsRL()[0][2][1] +"</td><td>"+ this.eService.getStatsRL()[0][2][2] +"</td><td>"+ this.eService.getStatsRL()[0][2][3] +"</td></tr></tbody></table>"
+      this.htmlYouWantToAdd += "<br><h5>"+ this.eService.getStatsRL()[3] +"</h5><table class=\"table\"><thead><tr><th scope=\"col\">Player</th><th scope=\"col\">Shots</th><th scope=\"col\">Saves</th><th scope=\"col\">Goals</th></tr></thead><tbody><tr><th scope=\"row\">"+ this.eService.getStatsRL()[1][0][0] +"</th><td>"+ this.eService.getStatsRL()[1][0][1] +"</td><td>"+ this.eService.getStatsRL()[1][0][2] +"</td><td>"+ this.eService.getStatsRL()[1][0][3] +"</td></tr><tr><th scope=\"row\">"+ this.eService.getStatsRL()[1][1][0] +"</th><td>"+ this.eService.getStatsRL()[1][1][1] +"</td><td>"+ this.eService.getStatsRL()[1][1][2] +"</td><td>"+ this.eService.getStatsRL()[1][1][3] +"</td></tr><tr><th scope=\"row\">"+ this.eService.getStatsRL()[1][2][0] +"</th><td>"+ this.eService.getStatsRL()[1][2][1] +"</td><td>"+ this.eService.getStatsRL()[1][2][2] +"</td><td>"+ this.eService.getStatsRL()[1][2][3] +"</td></tr></tbody></table>"
+    }
+    else if(this.eService.EVENTS[0].game == "Valorant"){
+      this.htmlYouWantToAdd = "<table class=\"table\"><thead><tr><th scope=\"col\">Player</th><th scope=\"col\">Shots</th><th scope=\"col\">Saves</th><th scope=\"col\">Goals</th></tr></thead><tbody><tr><th scope=\"row\">"+ this.eService.getStatsRL()[0][0] +"</th><td>"+ this.eService.getStatsRL()[0][1] +"</td><td>"+ this.eService.getStatsRL()[0][2] +"</td><td>"+ this.eService.getStatsRL()[0][3] +"</td></tr><tr><th scope=\"row\">"+ this.eService.getStatsRL()[1][0] +"</th><td>"+ this.eService.getStatsRL()[1][1] +"</td><td>"+ this.eService.getStatsRL()[1][2] +"</td><td>"+ this.eService.getStatsRL()[1][3] +"</td></tr><tr><th scope=\"row\">"+ this.eService.getStatsRL()[2][0] +"</th><td>"+ this.eService.getStatsRL()[2][1] +"</td><td>"+ this.eService.getStatsRL()[2][2] +"</td><td>"+ this.eService.getStatsRL()[2][3] +"</td></tr></tbody></table>"
+    }
+    else if(this.eService.EVENTS[0].game == "Hockey"){
+      this.htmlYouWantToAdd = "<table class=\"table\"><thead><tr><th scope=\"col\">Player</th><th scope=\"col\">Shots</th><th scope=\"col\">Saves</th><th scope=\"col\">Goals</th></tr></thead><tbody><tr><th scope=\"row\">"+ this.eService.getStatsRL()[0][0] +"</th><td>"+ this.eService.getStatsRL()[0][1] +"</td><td>"+ this.eService.getStatsRL()[0][2] +"</td><td>"+ this.eService.getStatsRL()[0][3] +"</td></tr><tr><th scope=\"row\">"+ this.eService.getStatsRL()[1][0] +"</th><td>"+ this.eService.getStatsRL()[1][1] +"</td><td>"+ this.eService.getStatsRL()[1][2] +"</td><td>"+ this.eService.getStatsRL()[1][3] +"</td></tr><tr><th scope=\"row\">"+ this.eService.getStatsRL()[2][0] +"</th><td>"+ this.eService.getStatsRL()[2][1] +"</td><td>"+ this.eService.getStatsRL()[2][2] +"</td><td>"+ this.eService.getStatsRL()[2][3] +"</td></tr></tbody></table>"
+    }
+    else if(this.eService.EVENTS[0].game == "Basket Ball"){
+      this.htmlYouWantToAdd = "<table class=\"table\"><thead><tr><th scope=\"col\">Player</th><th scope=\"col\">Shots</th><th scope=\"col\">Saves</th><th scope=\"col\">Goals</th></tr></thead><tbody><tr><th scope=\"row\">"+ this.eService.getStatsRL()[0][0] +"</th><td>"+ this.eService.getStatsRL()[0][1] +"</td><td>"+ this.eService.getStatsRL()[0][2] +"</td><td>"+ this.eService.getStatsRL()[0][3] +"</td></tr><tr><th scope=\"row\">"+ this.eService.getStatsRL()[1][0] +"</th><td>"+ this.eService.getStatsRL()[1][1] +"</td><td>"+ this.eService.getStatsRL()[1][2] +"</td><td>"+ this.eService.getStatsRL()[1][3] +"</td></tr><tr><th scope=\"row\">"+ this.eService.getStatsRL()[2][0] +"</th><td>"+ this.eService.getStatsRL()[2][1] +"</td><td>"+ this.eService.getStatsRL()[2][2] +"</td><td>"+ this.eService.getStatsRL()[2][3] +"</td></tr></tbody></table>"
+    }
   }
 
   // @Input() shots: number = this.getTotalShots();
